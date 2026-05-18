e575c3e150289e98e89b6912a6c4aa6ee5ee0092
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2026-05-18 17:34:32 +0700

    add class lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 class seseorang {
-public
+public:
     virtual void pesan() = 0;
     // virtual void pesan(){
     //      cout<<"Pesan dari seseorang"<<endl;
     //}
+};
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
 };
